1050158701dd98bae85b4f9fe469e19e1fc675ba
bprlab <bprlab@10.200.144.35>
2019-04-27 12:49:20 +0300

    Ders9Ornek

---
+++ ders9Ornek/src/main/java/tr/edu/medipol/yazilimarac/ders9Ornek/Ders9OrnekApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimarac.ders9Ornek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders9OrnekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders9OrnekApplication.class, args);
+	}
+
+}
